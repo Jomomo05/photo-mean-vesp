df9820cde0b11b2840058a32d3d94500a1b68a0d
Jose Miguel <a00227714@tec.mx>
2023-05-31 14:55:20 -0600

    base image photo form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoFormComponent,
+    NavigationComponent,
+    PhotoListComponent,
+    PhotoPreviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
