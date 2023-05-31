0d20ab8b0a40d0fbe357beaafa957042f372bf56
Jose Miguel <a00227714@tec.mx>
2023-05-31 15:17:04 -0600

    actual code

--- src/app/components/photo-form/photo-form.component.ts
+++ src/app/components/photo-form/photo-form.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router } from "@angular/router";
+import {PhotoService} from "../../services/photo.service";
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
   styleUrls: ['./photo-form.component.css']
 })
-export class PhotoFormComponent {
+export class PhotoFormComponent implements OnInit {
+
+  file!: File;
+  photoSelected!: ArrayBuffer | null;
+
+  constructor(private photoService:PhotoService, private router:Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onPhotoSelected(event: any): void{
+    if(this.file=event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      // Image preview
+      const reader = new FileReader();
+      reader.onload =(e: ProgressEvent<FileReader>) => {
+        this.photoSelected = e.target?.result as ArrayBuffer;
+      }
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  uploadPhoto(title:HTMLInputElement, description:HTMLTextAreaElement):boolean{
+    this.photoService.createPhoto(title.value, description.value, this.file)
+      .subscribe({
+        complete:()=>this.router.navigate(['/photos']),
+        error:(e)=> console.log(e)
+      });
 
 }
